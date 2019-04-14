9ddb369667d6e758ae993e35a201c446446cba11
EvaJohnson <37099960+aprilyolies@users.noreply.github.com>
2019-04-14 22:15:00 +0800

    Optimization Solution30.

--- src/main/java/com/eva/solution/leetcode/Solution30.java
+++ src/main/java/com/eva/solution/leetcode/Solution30.java
@@ -14,23 +14,22 @@ public class Solution30 {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> ans = new LinkedList<>();
         if (words == null || words.length == 0 || "".equals(s)) return ans;
-        int l = words[0].length() * words.length;
+        int len = words[0].length() * words.length;
         int wl = words[0].length();
         int sl = s.length();
         int wc = words.length;
         ArrayList<String> toVisit = new ArrayList<>(Arrays.asList(words));
         LinkedList<String> visited = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
         // 如果剩余的字符串小于串联字符串的总和，那就退出循环
-        for (int i = 0; i < sl - l + 1; i++) {
-            sb.append(s.substring(i));
+        for (int i = 0; i < sl - len + 1; i++) {
+            int l = i;
             for (int k = 0; k < wc; k++) {
                 int before = visited.size();
-                String subs = sb.substring(0, wl);
+                String subs = s.substring(l, l + wl);
                 for (int j = 0; j < toVisit.size(); j++) {
                     if (subs.equals(toVisit.get(j))) {
                         visited.add(toVisit.remove(j));
-                        sb.delete(0, wl);
+                        l += wl;
                         break;
                     }
                 }
@@ -40,7 +39,6 @@ public class Solution30 {
             }
             toVisit.addAll(visited);
             visited.clear();
-            sb.delete(0, sb.length());
         }
         return ans;
     }
