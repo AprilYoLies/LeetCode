db4c158b741f94ea60f00de369b61b625b17e9db
AprilYoLies <863821569@qq.com>
2019-08-04 11:37:36 +0800

    借助 ReentrantLock 生成的 Condition 类实现有界 buffer，通过测试

---
+++ src/test/java/com/eva/leetcode/BoundedBuffer.java
@@ -0,0 +1,69 @@
+package com.eva.leetcode;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Author EvaJohnson
+ * @Date 2019-08-04
+ * @Email g863821569@gmail.com
+ */
+public class BoundedBuffer<T> {
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition isFull = lock.newCondition();
+    private Condition isEmpty = lock.newCondition();
+
+    private Object[] arrays;
+
+    private int idx = 0;
+
+    private BoundedBuffer(int vol) {
+        //noinspection unchecked
+        this.arrays = new Object[vol + 1];
+    }
+
+    private T get() {
+        try {
+            lock.lock();
+            if (idx == -1) {
+                isEmpty.await();
+            }
+            isFull.signal();
+            //noinspection unchecked
+            return (T) arrays[idx--];
+        } catch (InterruptedException ignored) {
+
+        } finally {
+            lock.unlock();
+        }
+        return null;
+    }
+
+    private void put(T val) {
+        try {
+            lock.lock();
+            if (idx == arrays.length - 1) {
+                isFull.await();
+            }
+            arrays[++idx] = val;
+            isEmpty.signal();
+        } catch (InterruptedException ignored) {
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        BoundedBuffer<Integer> buffer = new BoundedBuffer<>(10);
+        new Thread(() -> {
+            for (int i = 1; i < 1000; i++) {
+                buffer.put(i);
+                System.out.println("Put " + i);
+            }
+        }).start();
+        for (; ; ) {
+            Thread.sleep(1000);
+            buffer.get();
+        }
+    }
+}
