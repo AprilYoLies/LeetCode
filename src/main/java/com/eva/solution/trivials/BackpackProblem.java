a64edb71d6f5ae41f4bd199e12a128ae17589e29
AprilYoLies <863821569@qq.com>
2019-08-15 19:53:18 +0800

    剑指 offer：0 - 1 背包问题

---
+++ src/main/java/com/eva/solution/trivials/BackpackProblem.java
@@ -0,0 +1,64 @@
+package com.eva.solution.trivials;
+
+import java.util.Arrays;
+
+/**
+ * @Author EvaJohnson
+ * @Date 2019-08-15
+ * @Email g863821569@gmail.com
+ */
+public class BackpackProblem {
+    // W 为背包能容纳的总重量
+    // N 为物品数量
+    // weights 数组存储 N 个物品的重量
+    // values 数组存储 N 个物品的价值
+    public static int knapsack1(int W, int N, int[] weights, int[] values) {
+        int[][] dp = new int[N + 1][W + 1];
+        for (int i = 1; i <= N; i++) {
+            int w = weights[i - 1], v = values[i - 1];
+            for (int j = 1; j <= W; j++) {
+                if (j >= w) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[N][W];
+    }
+
+    /**
+     * @param m 表示背包的最大容量
+     * @param n 表示商品个数
+     * @param w 表示商品重量数组
+     * @param p 表示商品价值数组
+     */
+    public static int[][] knapsack(int m, int n, int[] w, int[] p) {
+        //c[i][v]表示前i件物品恰放入一个重量为m的背包可以获得的最大价值
+        int c[][] = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++)
+            c[i][0] = 0;
+        for (int j = 0; j < m + 1; j++)
+            c[0][j] = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                //当物品为i件重量为j时，如果第i件的重量(w[i-1])小于重量j时，c[i][j]为下列两种情况之一：
+                //(1)物品i不放入背包中，所以c[i][j]为c[i-1][j]的值
+                //(2)物品i放入背包中，则背包剩余重量为j-w[i-1],所以c[i][j]为c[i-1][j-w[i-1]]的值加上当前物品i的价值
+                if (w[i - 1] <= j) {
+                    if (c[i - 1][j] < (c[i - 1][j - w[i - 1]] + p[i - 1]))
+                        c[i][j] = c[i - 1][j - w[i - 1]] + p[i - 1];
+                    else
+                        c[i][j] = c[i - 1][j];
+                } else
+                    c[i][j] = c[i - 1][j];
+            }
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(knapsack(10, 3, new int[]{3, 4, 5}, new int[]{4, 5, 6})));
+    }
+}
