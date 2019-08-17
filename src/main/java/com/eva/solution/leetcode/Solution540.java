18b50d29dc2aae4fc916ca91ebf016237fcd15c5
AprilYoLies <863821569@qq.com>
2019-08-17 17:29:50 +0800

    leetcode：有序数组的 Single Element，二分查找实现，简化边界判断方法

--- src/main/java/com/eva/solution/leetcode/Solution540.java
+++ src/main/java/com/eva/solution/leetcode/Solution540.java
@@ -9,28 +9,36 @@ public class Solution540 {
     public static int singleNonDuplicate(int[] nums) {
         if (nums.length == 1)
             return nums[0];
-        int l = 0, r = nums.length - 1;
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[nums.length - 1] != nums[nums.length - 2])
+            return nums[nums.length - 1];
+        int l = 1, r = nums.length - 2;
         return binarySearch(nums, l, r);
     }
 
+    /**
+     * 二分查找函数
+     *
+     * @param nums 数组
+     * @param l    左边界
+     * @param r    右边界
+     * @return 目标数
+     */
     private static int binarySearch(int[] nums, int l, int r) {
         if (l >= r) {
-            if (r == 0) return nums[0];
-            if (r == nums.length - 1) return nums[nums.length - 1];
             if (nums[r] != nums[r - 1] && nums[r] != nums[r + 1])
                 return nums[r];
             if (nums[r - 1] != nums[r - 2] && nums[r - 1] != nums[r])
                 return nums[r];
             return nums[l];
         }
-        int mid = (l + r) >>> 1;
-        if (mid == 0) return nums[0];
-        if (mid == nums.length - 1) return nums[nums.length - 1];
-        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+        int mid = (l + r) >>> 1;    // 中间数的索引
+        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])   // 如果中间数是单个数（就是我们要找的数），直接返回
             return nums[mid];
         int pos = mid;
-        if (nums[pos] != nums[pos - 1])
-            pos++;
+        if (nums[pos] != nums[pos - 1]) // 这里成立的话，说明 mid 不是成对数的第二个
+            pos++;  // 那么就定位到第二个
         if ((pos & 1) != 0) {   // 奇数 查找右边
             return binarySearch(nums, mid + 1, r);
         } else {    // 偶数 查找左边
