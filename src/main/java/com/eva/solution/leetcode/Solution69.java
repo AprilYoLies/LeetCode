0933ec994600a58bb92188f1b2358ad31eb1b4ee
AprilYoLies <863821569@qq.com>
2019-08-17 13:05:44 +0800

    Leetcode：x 的平方根，二分查找优化实现

--- src/main/java/com/eva/solution/leetcode/Solution69.java
+++ src/main/java/com/eva/solution/leetcode/Solution69.java
@@ -31,10 +31,35 @@ public class Solution69 {
         return (int) (pow < x ? l : r);
     }
 
+    public static int mySqrt1(int x) {
+        if (x <= 1)
+            return x;
+        int l = 1, r = x;
+        while (l < r) {
+            int mid = (l + r) >>> 1;
+            int div = x / mid;
+            if (div == mid)
+                return mid;
+            else if (div > mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        long pow = (long)l * l;
+        if (l == r) {
+            if (pow > x)
+                return l - 1;
+            return l;
+        }
+        return pow < x ? l : r;
+    }
+
     public static void main(String[] args) {
-        System.out.println(mySqrt(2147395599));
+        System.out.println(mySqrt1(2147483647));
         for (int i = 0; i < 100; i++) {
             System.out.println(i + " - " + mySqrt(i));
+            System.out.println(i + " - " + mySqrt1(i));
         }
     }
 }
