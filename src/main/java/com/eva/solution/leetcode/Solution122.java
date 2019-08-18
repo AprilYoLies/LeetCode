c7352232119ece58d763656f8f8acdf956df6698
AprilYoLies <863821569@qq.com>
2019-08-18 20:37:29 +0800

    leetcode：最佳买卖股票时机含冷冻期，动态规划实现

--- src/main/java/com/eva/solution/leetcode/Solution122.java
+++ src/main/java/com/eva/solution/leetcode/Solution122.java
@@ -14,9 +14,8 @@ public class Solution122 {
         dp[1][0] = 0;
         dp[1][1] = -prices[0];
         for (int i = 2; i < prices.length + 1; i++) {
-            int temp = dp[i - 1][0];
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
-            dp[i][1] = Math.max(dp[i - 1][1], temp - prices[i - 1]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1]);
         }
         return dp[prices.length][0];
     }
--- src/main/java/com/eva/solution/leetcode/Solution309.java
+++ src/main/java/com/eva/solution/leetcode/Solution309.java
@@ -7,23 +7,20 @@ package com.eva.solution.leetcode;
  */
 public class Solution309 {
     // dp[i][0] = max(dp[i - 1][0],dp[i - 1][1] + price[i])
-    public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
+    public static int maxProfit(int[] prices) {
+        if (prices.length <= 1)
             return 0;
+        int[][] dp = new int[prices.length + 1][2];
+        dp[1][0] = 0;
+        dp[1][1] = -prices[0];
+        for (int i = 2; i < prices.length + 1; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i - 1]);
         }
-        int N = prices.length;
-        int[] buy = new int[N];
-        int[] s1 = new int[N];
-        int[] sell = new int[N];
-        int[] s2 = new int[N];
-        s1[0] = buy[0] = -prices[0];
-        sell[0] = s2[0] = 0;
-        for (int i = 1; i < N; i++) {
-            buy[i] = s2[i - 1] - prices[i];
-            s1[i] = Math.max(buy[i - 1], s1[i - 1]);
-            sell[i] = Math.max(buy[i - 1], s1[i - 1]) + prices[i];
-            s2[i] = Math.max(s2[i - 1], sell[i - 1]);
-        }
-        return Math.max(sell[N - 1], s2[N - 1]);
+        return dp[prices.length][0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{1, 2, 3, 0, 2}));
     }
 }
