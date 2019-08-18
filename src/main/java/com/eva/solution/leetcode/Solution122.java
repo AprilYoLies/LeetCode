c3253dade9c92528240128403c495266a3041d83
AprilYoLies <863821569@qq.com>
2019-08-18 13:50:22 +0800

    leetcode：买卖股票的最佳时机 II，购买股票次数不限，动态规划实现

--- src/main/java/com/eva/solution/leetcode/Solution122.java
+++ src/main/java/com/eva/solution/leetcode/Solution122.java
@@ -8,14 +8,15 @@ package com.eva.solution.leetcode;
 public class Solution122 {
     // dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + price[i])
     public static int maxProfit(int[] prices) {
+        if (prices.length <= 1)
+            return 0;
         int[][] dp = new int[prices.length + 1][2];
-        for (int i = 1; i < prices.length + 1; i++) {
-            for (int j = 0; j < 2; j++) {
-                if (j == 0)
-                    dp[i][j] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
-                else
-                    dp[i][j] = Math.max(dp[i - 1][0] - prices[i - 1], dp[i - 1][1]);
-            }
+        dp[1][0] = 0;
+        dp[1][1] = -prices[0];
+        for (int i = 2; i < prices.length + 1; i++) {
+            int temp = dp[i - 1][0];
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+            dp[i][1] = Math.max(dp[i - 1][1], temp - prices[i - 1]);
         }
         return dp[prices.length][0];
     }
