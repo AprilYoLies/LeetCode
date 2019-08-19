8f9054745abcc5350d2822914a2ae9a8d06caa53
AprilYoLies <863821569@qq.com>
2019-08-19 15:07:36 +0800

    LeetCode：给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。

---
+++ src/main/java/com/eva/solution/leetcode/Solution72.java
@@ -0,0 +1,30 @@
+package com.eva.solution.leetcode;
+
+/**
+ * @Author EvaJohnson
+ * @Date 2019-08-19
+ * @Email g863821569@gmail.com
+ */
+public class Solution72 {
+    public static int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++) {
+            for (int j = 0; j <= word2.length(); j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = i + j;
+                    continue;
+                }
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1]);
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1), dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minDistance("horse", "ros"));
+    }
+}
