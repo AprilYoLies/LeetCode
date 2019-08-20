7b9a9ecd33fcc1147319d09d224a7c8db2fd74a8
AprilYoLies <863821569@qq.com>
2019-08-20 15:28:21 +0800

    LeetCode：删除链表的倒数第N个节点

--- src/main/java/com/eva/solution/leetcode/Solution19.java
+++ src/main/java/com/eva/solution/leetcode/Solution19.java
@@ -6,7 +6,7 @@ package com.eva.solution.leetcode;
  * @Email g863821569@gmail.com
  */
 public class Solution19 {
-    public ListNode removeNthFromEnd(ListNode head, int n) {
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
         ListNode cur = head;
         ListNode target = head;
         while (cur != null) {
@@ -22,5 +22,40 @@ public class Solution19 {
         }
         target.next = target.next.next;
         return head;
+    }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode cur = head, target = null, pre = null;
+        int count = 1;
+        if (count == n) {
+            target = cur;
+        }
+        while (cur.next != null) {
+            count++;
+            if (count == n) {
+                target = head;
+            } else if (count > n) {
+                pre = target;
+                target = target.next;
+            }
+            cur = cur.next;
+        }
+        if (pre == null)
+            return head.next;
+        pre.next = pre.next.next;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+//        ListNode node1 = new ListNode(2);
+//        ListNode node2 = new ListNode(3);
+//        ListNode node3 = new ListNode(4);
+//        ListNode node4 = new ListNode(5);
+//        head.next = node1;
+//        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+        removeNthFromEnd(head, 1);
     }
 }
