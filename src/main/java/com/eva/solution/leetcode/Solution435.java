7a75d52e8b73cc828f969ec0a448b81978d9480b
AprilYoLies <863821569@qq.com>
2019-08-20 22:57:25 +0800

    LeetCode：无重叠区间，贪心算法实现

---
+++ src/main/java/com/eva/solution/leetcode/Solution435.java
@@ -0,0 +1,43 @@
+package com.eva.solution.leetcode;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution435 {
+    public static int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        int count = 1;
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= end) {
+                count++;
+                end = intervals[i][1];
+            }
+        }
+        return intervals.length - count;
+    }
+
+    public static int eraseOverlapIntervals1(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        int cnt = 1;
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                continue;
+            }
+            end = intervals[i][1];
+            cnt++;
+        }
+        return intervals.length - cnt;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2}, {1, 2}, {1, 2}}));
+    }
+}
