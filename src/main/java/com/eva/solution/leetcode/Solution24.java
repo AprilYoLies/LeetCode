d9138e44bf238c16b1130d8db2b73b07159a2d0a
AprilYoLies <863821569@qq.com>
2019-08-20 17:33:07 +0800

    LeetCode：两两交换链表中的节点

--- src/main/java/com/eva/solution/leetcode/Solution24.java
+++ src/main/java/com/eva/solution/leetcode/Solution24.java
@@ -6,7 +6,7 @@ package com.eva.solution.leetcode;
  * @Email g863821569@gmail.com
  */
 public class Solution24 {
-    public ListNode swapPairs(ListNode head) {
+    public ListNode swapPairs1(ListNode head) {
         if (head == null)
             return null;
         if (head.next == null)
@@ -27,5 +27,40 @@ public class Solution24 {
             else break;
         }
         return head;
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode nHead = head.next, cur = head, next, aNext, pre = null;
+        while (cur != null) {
+            next = cur.next;
+            if (next == null) {
+                break;
+            }
+            aNext = next.next;
+            next.next = cur;
+            cur.next = aNext;
+            if (pre != null)
+                pre.next = next;
+            pre = cur;
+            cur = aNext;
+        }
+        return nHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(3);
+//        ListNode node3 = new ListNode(4);
+//        ListNode node4 = new ListNode(5);
+//        ListNode node5 = new ListNode(6);
+        head.next = node1;
+//        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node5;
+        swapPairs(head);
     }
 }
