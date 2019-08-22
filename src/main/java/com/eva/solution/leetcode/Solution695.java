e57b02bd6870dd00c13837392d75e3f7e7033d8f
AprilYoLies <863821569@qq.com>
2019-08-22 22:37:25 +0800

    LeetCode：岛屿的最大面积，深度优先搜索实现

--- src/main/java/com/eva/solution/leetcode/Solution695.java
+++ src/main/java/com/eva/solution/leetcode/Solution695.java
@@ -4,7 +4,7 @@ public class Solution695 {
     private static int m, n;
     private static int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    public static int maxAreaOfIsland(int[][] grid) {
+    public static int maxAreaOfIsland1(int[][] grid) {
         if (grid == null || grid.length == 0) {
             return 0;
         }
@@ -27,6 +27,30 @@ public class Solution695 {
         int area = 1;
         for (int[] d : direction) {
             area += dfs(grid, r + d[0], c + d[1]);
+        }
+        return area;
+    }
+
+    public static int maxAreaOfIsland(int[][] grid) {
+        int r = grid.length, c = grid[0].length;
+        int max = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 0) continue;
+                int maybeMax = calAreaSize(grid, i, j);
+                if (maybeMax > max) max = maybeMax;
+            }
+        }
+        return max;
+    }
+
+    private static int calAreaSize(int[][] grid, int i, int j) {
+        if (i < 0 || i == grid.length || j < 0 || j == grid[0].length || grid[i][j] == 0) return 0;
+        int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        grid[i][j] = 0;
+        int area = 1;
+        for (int[] direction : directions) {
+            area += calAreaSize(grid, i + direction[0], j + direction[1]);
         }
         return area;
     }
