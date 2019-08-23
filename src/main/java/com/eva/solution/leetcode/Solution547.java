08f6c781627106228b41ba0673da710b24777de0
AprilYoLies <863821569@qq.com>
2019-08-23 09:51:42 +0800

    leetcode：求朋友圈个数，深度优先搜索实现

--- src/main/java/com/eva/solution/leetcode/Solution547.java
+++ src/main/java/com/eva/solution/leetcode/Solution547.java
@@ -3,7 +3,7 @@ package com.eva.solution.leetcode;
 public class Solution547 {
     private static int n;
 
-    public static int findCircleNum(int[][] M) {
+    public static int findCircleNum1(int[][] M) {
         n = M.length;
         int circleNum = 0;
         boolean[] hasVisited = new boolean[n];
@@ -25,7 +25,33 @@ public class Solution547 {
         }
     }
 
+    public static int findCircleNum(int[][] M) {
+        boolean[] visited = new boolean[M.length];
+        int friendsZone = 0;
+
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i]) continue;
+            friendsZone++;
+            findFriends(M, i, visited);
+        }
+        return friendsZone;
+    }
+
+    private static void findFriends(int[][] m, int cur, boolean[] visited) {
+        if (visited[cur]) return;
+        visited[cur] = true;
+        for (int i = 0; i < m.length; i++) {
+            if (m[cur][i] == 1)
+                findFriends(m, i, visited);
+        }
+    }
+
     public static void main(String[] args) {
+        System.out.println(findCircleNum1(new int[][]{
+                {1, 1, 0},
+                {1, 1, 0},
+                {0, 0, 1}}
+        ));
         System.out.println(findCircleNum(new int[][]{
                 {1, 1, 0},
                 {1, 1, 0},
