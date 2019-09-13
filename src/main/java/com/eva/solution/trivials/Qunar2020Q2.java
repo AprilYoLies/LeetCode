098456c5cb51efcf4ba18adcb687875092762e08
AprilYoLies <863821569@qq.com>
2019-09-13 12:31:52 +0800

    最大子矩阵和的问题

--- src/main/java/com/eva/solution/trivials/Qunar2020Q2.java
+++ src/main/java/com/eva/solution/trivials/Qunar2020Q2.java
@@ -7,6 +7,11 @@ import java.util.Scanner;
  * @Date 2019-09-12
  * @Email g863821569@gmail.com
  */
+
+/**
+ * 4
+ * 0 -2 -7 0 9 2 -6 2 -4 1 -4 1 -1 8 0 -2
+ */
 public class Qunar2020Q2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,32 +26,28 @@ public class Qunar2020Q2 {
     }
 
     public static int maxSubArray(int[][] arr, int n) {
-        int[] temp = new int[n];
-        for (int i = 0; i < n; i++)
-            temp[i] = 0;
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
-            temp = arr[i];
-            max = Math.max(maxSubArray(temp, n), max);
-            for (int j = i + 1; j < n; j++) {
+            int[] sum = new int[n];
+            for (int j = i; j < n; j++) {
                 for (int k = 0; k < n; k++) {
-                    temp[k] += arr[j][k];
+                    sum[k] += arr[j][k];
                 }
-                max = Math.max(maxSubArray(temp, n), max);
+                max = Math.max(maxSubArray(sum, n), max);
             }
         }
         return max;
     }
 
     public static int maxSubArray(int[] array, int n) {
-        int sum = 0;
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            sum += array[i];
-            if (sum > max)
-                max = sum;
-            if (sum < 0)
-                sum = 0;
+        int[] dp = new int[n];
+        dp[0] = array[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + array[i], array[i]);
+        }
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            if (dp[i] > max) max = dp[i];
         }
         return max;
     }
