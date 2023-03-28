c423c7911d0859bfae3be69db0ae6659ed8ecded
eva <863821569@qq.com>
2023-03-28 23:27:30 +0800

    leetcode 347

---
+++ src/main/java/com/eva/solution/leetcode/Solution347.java
@@ -0,0 +1,59 @@
+package com.eva.solution.leetcode;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @Author EvaJohnson
+ * @Date 2023-03-28 22:44:46
+ * @Email g863821569@gmail.com
+ */
+public class Solution347 {
+
+    @Test
+    public void testTopKFrequent() {
+        int[] nums = new int[]{4, 1, -1, 2, -1, 2, 3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums, k)));
+    }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.compute(num, (key, value) -> value == null ? 1 : value + 1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((o1, o2) -> o2.getValue() - o1.getValue());
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            queue.offer(entry);
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll().getKey();
+        }
+        return ans;
+    }
+
+    public int[] topKFrequent1(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.compute(num, (key, value) -> value == null ? 1 : value + 1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (queue.size() < k) {
+                queue.offer(entry);
+            } else {
+                if (queue.peek().getValue() < entry.getValue()) {
+                    queue.poll();
+                    queue.offer(entry);
+                }
+            }
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = queue.poll().getKey();
+        }
+        return ans;
+    }
+}
