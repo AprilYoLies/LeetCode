f4336efc926e6e74d5ca9f601ce31a479daa0b29
eva <863821569@qq.com>
2023-04-03 22:26:23 +0800

    leetcode 22

--- src/main/java/com/eva/solution/leetcode/Solution22.java
+++ src/main/java/com/eva/solution/leetcode/Solution22.java
@@ -1,5 +1,7 @@
 package com.eva.solution.leetcode;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -11,7 +13,72 @@ import java.util.Set;
  * @Email g863821569@gmail.com
  */
 public class Solution22 {
+
+    @Test
+    public void testSolution() {
+        System.out.println(generateParenthesis(3));
+    }
+
     public List<String> generateParenthesis(int n) {
+        StringBuilder sb = new StringBuilder();
+        List<String> ans = new ArrayList<>();
+        generateParenthesis(n * 2, sb, ans);
+        return ans;
+    }
+
+    public void generateParenthesis(int len, StringBuilder sb, List<String> ans) {
+        if (sb.length() == len) {
+            String str = sb.toString();
+            if (validate(str)) {
+                ans.add(str);
+            }
+            return;
+        }
+        sb.append("(");
+        generateParenthesis(len, sb, ans);
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(")");
+        generateParenthesis(len, sb, ans);
+        sb.deleteCharAt(sb.length() - 1);
+    }
+
+    private boolean validate(String str) {
+        char[] chars = str.toCharArray();
+        int flag = 0;
+        for (char aChar : chars) {
+            if ('(' == aChar) {
+                flag++;
+            } else {
+                flag--;
+            }
+            if (flag < 0) return false;
+        }
+        return flag == 0;
+    }
+
+    public List<String> generateParenthesis2(int n) {
+        Set<String> set = new HashSet<>();
+        set.add("()");
+        for (int i = 0; i < n - 1; i++) {
+            addParenthesis(set);
+        }
+        return new ArrayList<>(set);
+    }
+
+    private void addParenthesis(Set<String> set) {
+        List<String> tmp = new ArrayList<>(set);
+        set.clear();
+        for (String cur : tmp) {
+            StringBuilder sb = new StringBuilder(cur);
+            for (int i = 0; i <= sb.length(); i++) {
+                sb.insert(i, "()");
+                set.add(sb.toString());
+                sb.delete(i, i + 2);
+            }
+        }
+    }
+
+    public List<String> generateParenthesis1(int n) {
         Set<String> set = new HashSet<>();
         set.add("()");
         for (int i = 0; i < n - 1; i++) {
