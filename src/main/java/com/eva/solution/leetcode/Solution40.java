a5c651ec4d710d904cf033fa390ee0f28ff84324
eva <863821569@qq.com>
2023-04-06 23:09:18 +0800

    leetcode 40（回溯）

--- src/main/java/com/eva/solution/leetcode/Solution40.java
+++ src/main/java/com/eva/solution/leetcode/Solution40.java
@@ -8,41 +8,41 @@ import java.util.List;
 
 /**
  * @Author EvaJohnson
- * @Date 2023-04-04 19:04:18
+ * @Date 2023-04-06 23:06:18
  * @Email g863821569@gmail.com
  */
-public class Solution39 {
+public class Solution40 {
 
     @Test
     public void testSolution() {
         int[] candidates = new int[]{2, 3, 6, 7};
         int target = 7;
-        System.out.println(combinationSum(candidates, target));
+        System.out.println(combinationSum2(candidates, target));
     }
 
     private final List<List<Integer>> ans = new ArrayList<>();
 
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<Integer> item = new ArrayList<>();
-        combinationSum(candidates, target, item, 0, 0);
+        combinationSum2(candidates, target, item, 0, 0);
         return ans;
     }
 
-
-    public void combinationSum(int[] candidates, int target, List<Integer> item, int count, int pos) {
+    public void combinationSum2(int[] candidates, int target, List<Integer> item, int count, int pos) {
         if (count == target) {
             ans.add(new ArrayList<>(item));
             return;
         }
         for (int i = pos; i < candidates.length; i++) {
+            if (i > pos && candidates[i] == candidates[i - 1]) continue;
             item.add(candidates[i]);
             count += candidates[i];
             if (count > target) {
                 item.remove(item.size() - 1);
                 break;
             }
-            combinationSum(candidates, target, item, count, i);
+            combinationSum2(candidates, target, item, count, i + 1);
             item.remove(item.size() - 1);
             count -= candidates[i];
         }
