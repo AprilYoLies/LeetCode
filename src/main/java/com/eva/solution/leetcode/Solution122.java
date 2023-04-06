cae592cbb6d0b5a23f64c0b4c4b04ebf976c700b
eva <863821569@qq.com>
2023-04-07 00:14:00 +0800

    leetcode 122（贪心）

--- src/main/java/com/eva/solution/leetcode/Solution122.java
+++ src/main/java/com/eva/solution/leetcode/Solution122.java
@@ -1,15 +1,43 @@
 package com.eva.solution.leetcode;
 
+import org.junit.Test;
+
 /**
  * @Author EvaJohnson
  * @Date 2019-08-18
  * @Email g863821569@gmail.com
  */
 public class Solution122 {
+
+    @Test
+    public void testSolution() {
+        System.out.println(maxProfit(new int[]{1}));
+    }
+
+    public int maxProfit(int[] prices) {
+        int income = 0;
+        int buy = 0;
+        boolean hasBuy = false;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1] && !hasBuy) {
+                buy = prices[i - 1];
+                hasBuy = true;
+            }
+            if (hasBuy && (prices[i] < prices[i - 1] || i == prices.length - 1)) {
+                int profit = Math.max(prices[i], prices[i - 1]) - buy;
+                if (profit > 0) {
+                    income += profit;
+                }
+                hasBuy = false;
+            }
+        }
+        return income;
+    }
+
+
     // dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + price[i])
-    public static int maxProfit(int[] prices) {
-        if (prices.length <= 1)
-            return 0;
+    public static int maxProfit1(int[] prices) {
+        if (prices.length <= 1) return 0;
         int[][] dp = new int[prices.length + 1][2];
         dp[1][0] = 0;
         dp[1][1] = -prices[0];
@@ -21,6 +49,6 @@ public class Solution122 {
     }
 
     public static void main(String[] args) {
-        System.out.println(maxProfit(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(maxProfit1(new int[]{1, 2, 3, 4, 5}));
     }
 }
