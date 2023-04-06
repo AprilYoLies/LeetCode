e688a94321b170b03191607c16cebd3370a65783
eva <863821569@qq.com>
2023-04-06 23:05:32 +0800

    leetcode 695（DFS）

--- src/main/java/com/eva/solution/leetcode/Solution695.java
+++ src/main/java/com/eva/solution/leetcode/Solution695.java
@@ -1,6 +1,49 @@
 package com.eva.solution.leetcode;
 
+import org.junit.Test;
+
 public class Solution695 {
+
+    @Test
+    public void testSolution() {
+        int[][] grid = {
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
+        System.out.println(maxAreaOfIsland(grid));
+    }
+
+    private final int[][] directions = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int r = grid.length, c = grid[0].length;
+        int max = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 0) continue;
+                int area = dfs(grid, i, j);
+                if (area > max) max = area;
+            }
+        }
+        return max;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i == grid.length || j < 0 || j == grid[0].length || grid[i][j] == 0) return 0;
+        grid[i][j] = 0;
+        int area = 1;
+        for (int[] direction : directions) {
+            area += dfs(grid, i + direction[0], j + direction[1]);
+        }
+        return area;
+    }
+
     private static int m, n;
     private static int[][] direction = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
@@ -13,25 +56,25 @@ public class Solution695 {
         int maxArea = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                maxArea = Math.max(maxArea, dfs(grid, i, j));
+                maxArea = Math.max(maxArea, dfs1(grid, i, j));
             }
         }
         return maxArea;
     }
 
-    private static int dfs(int[][] grid, int r, int c) {
+    private static int dfs1(int[][] grid, int r, int c) {
         if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0) {
             return 0;
         }
         grid[r][c] = 0;
         int area = 1;
         for (int[] d : direction) {
-            area += dfs(grid, r + d[0], c + d[1]);
+            area += dfs1(grid, r + d[0], c + d[1]);
         }
         return area;
     }
 
-    public static int maxAreaOfIsland(int[][] grid) {
+    public static int maxAreaOfIsland2(int[][] grid) {
         int r = grid.length, c = grid[0].length;
         int max = 0;
         for (int i = 0; i < r; i++) {
@@ -56,7 +99,7 @@ public class Solution695 {
     }
 
     public static void main(String[] args) {
-        System.out.println(maxAreaOfIsland(new int[][]{
+        System.out.println(maxAreaOfIsland2(new int[][]{
                 {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
                 {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
