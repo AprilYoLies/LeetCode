3416b65b51d947b3389b4a9a13e2c73c5f9d9bfa
eva <863821569@qq.com>
2023-04-10 22:55:24 +0800

    leetcode 435、452

--- src/main/java/com/eva/solution/leetcode/Solution435.java
+++ src/main/java/com/eva/solution/leetcode/Solution435.java
@@ -1,10 +1,34 @@
 package com.eva.solution.leetcode;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class Solution435 {
-    public static int eraseOverlapIntervals(int[][] intervals) {
+
+    @Test
+    public void testSolution() {
+        int[][] ratings = new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+        System.out.println(eraseOverlapIntervals(ratings));
+    }
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        int min = intervals[0][1];
+        int count = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < min) {
+                count++;
+                min = Math.min(min, intervals[i][1]);
+            } else {
+                min = intervals[i][1];
+            }
+        }
+        return count;
+    }
+
+    public static int eraseOverlapInterval2(int[][] intervals) {
         if (intervals.length == 0) {
             return 0;
         }
@@ -38,6 +62,6 @@ public class Solution435 {
     }
 
     public static void main(String[] args) {
-        System.out.println(eraseOverlapIntervals(new int[][]{{1, 2}, {1, 2}, {1, 2}}));
+        System.out.println(eraseOverlapInterval2(new int[][]{{1, 2}, {1, 2}, {1, 2}}));
     }
 }
--- src/main/java/com/eva/solution/leetcode/Solution452.java
+++ src/main/java/com/eva/solution/leetcode/Solution452.java
@@ -1,10 +1,34 @@
 package com.eva.solution.leetcode;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class Solution452 {
-    public static int findMinArrowShots(int[][] points) {
+
+    @Test
+    public void testSolution() {
+        int[][] ratings = new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+        System.out.println(findMinArrowShots(ratings));
+    }
+
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(o -> o[0]));
+        int min = points[0][1];
+        int count = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (points[i][0] <= min) {
+                min = Math.min(min, points[i][1]);
+            } else {
+                count++;
+                min = points[i][1];
+            }
+        }
+        return count;
+    }
+
+    public static int findMinArrowShots1(int[][] points) {
         if (points.length == 0) return 0;
         Arrays.sort(points, Comparator.comparingInt(o -> o[1]));
         int count = 1;
@@ -19,6 +43,6 @@ public class Solution452 {
     }
 
     public static void main(String[] args) {
-        System.out.println(findMinArrowShots(new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}}));
+        System.out.println(findMinArrowShots1(new int[][]{{1, 2}, {2, 3}, {3, 4}, {4, 5}}));
     }
 }
