33797206cbc8e876ab26c5f0251dedf915789a65
eva <863821569@qq.com>
2023-04-13 21:20:55 +0800

    leetcode 32（动态规划）

--- src/main/java/com/eva/solution/leetcode/Solution32.java
+++ src/main/java/com/eva/solution/leetcode/Solution32.java
@@ -1,12 +1,40 @@
 package com.eva.solution.leetcode;
 
+import org.junit.Test;
+
 /**
  * @Author EvaJohnson
  * @Date 2019-04-23
  * @Email g863821569@gmail.com
  */
 public class Solution32 {
+
+    @Test
+    public void testSolution() {
+        String str = "(()())())())())())()())";
+        System.out.println(longestValidParentheses(str));
+    }
+
     public int longestValidParentheses(String s) {
+        int[] dp = new int[s.length()];
+        int max = 0;
+        for (int i = 1; i < dp.length; i++) {
+            char aChar = s.charAt(i);
+            if (aChar == ')') {
+                if (s.charAt(i - 1) == '(') {
+                    dp[i] = (i - 2 >= 0 ? dp[i - 2] : 0) + 2;
+                } else {
+                    if (i - dp[i - 1] - 1 >= 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
+                        dp[i] = 2 + dp[i - 1] + (i - dp[i - 1] - 2 >= 0 ? dp[i - dp[i - 1] - 2] : 0);
+                    }
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+
+    public int longestValidParentheses1(String s) {
         int a = getLenght(s, '(');
         int b = getLenght(new StringBuilder(s).reverse().toString(), ')');
         return Math.max(a, b);
