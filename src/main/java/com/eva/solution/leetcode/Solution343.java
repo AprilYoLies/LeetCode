c8f20bae3392a7a525ba4ebb0100ca8b9183106b
eva <863821569@qq.com>
2023-04-15 15:10:10 +0800

    leetcode 343（动态规划）

--- src/main/java/com/eva/solution/leetcode/Solution343.java
+++ src/main/java/com/eva/solution/leetcode/Solution343.java
@@ -1,12 +1,32 @@
 package com.eva.solution.leetcode;
 
+import org.junit.Test;
+
 /**
  * @Author EvaJohnson
  * @Date 2019-08-13
  * @Email g863821569@gmail.com
  */
 public class Solution343 {
-    public static int integerBreak(int n) {
+
+    @Test
+    public void testSolution() {
+        int n = 20;
+        System.out.println(integerBreak(n));
+    }
+
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i / 2 + 1; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+
+    public static int integerBreak1(int n) {
         if (n == 2) return 1;
         if (n == 3) return 2;
         int[] ab = maxAb(n);
@@ -35,6 +55,6 @@ public class Solution343 {
     }
 
     public static void main(String[] args) {
-        System.out.println(integerBreak(9));
+        System.out.println(integerBreak1(9));
     }
 }
